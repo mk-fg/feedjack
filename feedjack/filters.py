5797cdaeadb23577cc85beb57fa717f46e1e55d0
Mike Kazantsev <mk.fraggod@gmail.com>
2010-08-21 20:28:51 +0000

    filters: commented out title_similarity stub

--- feedjack/filters.py
+++ feedjack/filters.py
@@ -13,21 +13,20 @@ regex_search_content = ft.partial(_regex_search, dissector=op.attrgetter('conten
 DEFAULT_SIMILARITY_THRESHOLD = 0.85
 DEFAULT_SIMILARITY_TIMESPAN = 7 * 24 * 3600
 
-from datetime import datetime, timedelta
+# from datetime import datetime, timedelta
 
-def title_similarity(post, parameter=None):
-	from feedjack.models import Post
-	if parameter:
-		parameter = map(op.methodcaller('strip'), parameter.split(',', 1))
-		threshold = parameter.pop()
-		try: threshold, timespan = parameter.pop(), threshold
-		except IndexError: timespan = DEFAULT_SIMILARITY_TIMESPAN
-		threshold, timespan = float(threshold), int(timespan)
-	else:
-		threshold, timespan = DEFAULT_SIMILARITY_THRESHOLD, DEFAULT_SIMILARITY_TIMESPAN
-	similar = Post.objects.filtered().similar(threshold, title=post.title)\
-		.filter(date_updated__gt=datetime.now() - timedelta(seconds=timespan)
-	similar = similar.filter(
-	return not bool(Post.objects.filtered().similar(threshold, title=post.title)\
-		)[:1])
-
+# def title_similarity(post, parameter=None):
+# 	from feedjack.models import Post
+# 	if parameter:
+# 		parameter = map(op.methodcaller('strip'), parameter.split(',', 1))
+# 		threshold = parameter.pop()
+# 		try: threshold, timespan = parameter.pop(), threshold
+# 		except IndexError: timespan = DEFAULT_SIMILARITY_TIMESPAN
+# 		threshold, timespan = float(threshold), int(timespan)
+# 	else:
+# 		threshold, timespan = DEFAULT_SIMILARITY_THRESHOLD, DEFAULT_SIMILARITY_TIMESPAN
+# 	similar = Post.objects.filtered().similar(threshold, title=post.title)\
+# 		.filter(date_updated__gt=datetime.now() - timedelta(seconds=timespan)
+# 	similar = similar.filter(
+# 	return not bool(Post.objects.filtered().similar(threshold, title=post.title)\
+# 		)[:1])
