a35e244050ac19def8d80ccc5bbffcf3800f8a83
Mike Kazantsev <mk.fraggod@gmail.com>
2011-03-31 02:35:40 +0000

    news-fold: added unfold capability

--- feedjack/static/feedjack/fern/fold.fern.coffee
+++ feedjack/static/feedjack/fern/fold.fern.coffee
@@ -2,7 +2,6 @@
 // TODO: localStorage cleanup, check it's space limits
 // TODO: jquery.ui animation
 // TODO: fix style for links (bg crap on hover)
-// TODO: unfold button
 ###
 
 $(document).ready ->
@@ -16,7 +15,7 @@ $(document).ready ->
 	folds_commit = -> localStorage.setItem(storage_key, JSON.stringify(folds))
 
 	/* Fold everything under the specified day-header */
-	fold_entries = (h1, fold=null) ->
+	fold_entries = (h1, fold=null, unfold=false) ->
 		h1 = $(h1)
 		ts_day = h1.data('timestamp')
 		ts_entry_max = 0
@@ -24,14 +23,16 @@ $(document).ready ->
 		h1.nextUntil('h1').children('.entry').each (idx, el) ->
 			entry = $(el)
 			ts = entry.data('timestamp')
-			if fold is false
+			if unfold is true
 				entry.children('.content').css('display', '')
-			else if folds[ts_day] >= ts
+			else if fold isnt false and folds[ts_day] >= ts
 				entry.children('.content').css('display', 'none')
 			ts_entry_max = ts if (not folds[ts_day]? or folds[ts_day] < ts) and ts > ts_entry_max
 
-		/* No newer entries for a given day */
-		if fold isnt false and (fold or ts_entry_max == 0)
+		/* Fold whole day */
+		if unfold is true
+			h1.nextUntil('h1').css('display', '')
+		else if fold isnt false and (fold or ts_entry_max == 0)
 			h1.nextUntil('h1').css('display', 'none')
 
 		[ts_day, ts_entry_max]
@@ -45,13 +46,29 @@ $(document).ready ->
 
 	/* Fold day */
 	$('.button_fold').click (ev) ->
-		[ts_day, ts_entry_max] = fold_entries($(ev.target).parent('h1'), true)
-		folds[ts_day] = Math.max(ts_entry_max, folds[ts_day] or 0)
+		h1 = $(ev.target).parent('h1')
+		[ts_day, ts_entry_max] = fold_entries(h1, false)
+		if ts_entry_max > 0
+			fold_entries(h1, true)
+			folds[ts_day] = Math.max(ts_entry_max, folds[ts_day] or 0)
+		else
+			fold_entries(h1, false, true)
+			delete folds[ts_day]
 		folds_commit()
 
 	/* Fold all */
 	$('.button_fold_all').click (ev) ->
-		$('h1.feed').each (idx, el) ->
-			[ts_day, ts_entry_max] = fold_entries(el, true)
-			folds[ts_day] = Math.max(ts_entry_max, folds[ts_day] or 0)
-			folds_commit()
+		ts_page_max = 0
+		h1s = $('h1.feed')
+		h1s.each (idx, el) ->
+			[ts_day, ts_entry_max] = fold_entries(el, false)
+			ts_page_max = Math.max(ts_page_max, ts_entry_max)
+		if ts_page_max > 0
+			h1s.each (idx, el) ->
+				[ts_day, ts_entry_max] = fold_entries(el, true)
+				folds[ts_day] = Math.max(ts_entry_max, folds[ts_day] or 0)
+		else
+			h1s.each (idx, el) ->
+				[ts_day, ts_entry_max] = fold_entries(el, false, true)
+				delete folds[ts_day]
+		folds_commit()
--- feedjack/static/feedjack/fern/fold.fern.js
+++ feedjack/static/feedjack/fern/fold.fern.js
@@ -3,7 +3,6 @@
   // TODO: localStorage cleanup, check it's space limits
   // TODO: jquery.ui animation
   // TODO: fix style for links (bg crap on hover)
-  // TODO: unfold button
   */  $(document).ready(function() {
     /* IE6? Fuck off */;    var fold_entries, folds, folds_commit, storage_key, url_media, url_site, _ref;
     if (typeof localStorage == "undefined" || localStorage === null) {
@@ -17,10 +16,13 @@
       return localStorage.setItem(storage_key, JSON.stringify(folds));
     };
     /* Fold everything under the specified day-header */;
-    fold_entries = function(h1, fold) {
+    fold_entries = function(h1, fold, unfold) {
       var ts_day, ts_entry_max;
       if (fold == null) {
         fold = null;
+      }
+      if (unfold == null) {
+        unfold = false;
       }
       h1 = $(h1);
       ts_day = h1.data('timestamp');
@@ -29,17 +31,19 @@
         var entry, ts;
         entry = $(el);
         ts = entry.data('timestamp');
-        if (fold === false) {
+        if (unfold === true) {
           entry.children('.content').css('display', '');
-        } else if (folds[ts_day] >= ts) {
+        } else if (fold !== false && folds[ts_day] >= ts) {
           entry.children('.content').css('display', 'none');
         }
         if ((!(folds[ts_day] != null) || folds[ts_day] < ts) && ts > ts_entry_max) {
           return ts_entry_max = ts;
         }
       });
-      /* No newer entries for a given day */;
-      if (fold !== false && (fold || ts_entry_max === 0)) {
+      /* Fold whole day */;
+      if (unfold === true) {
+        h1.nextUntil('h1').css('display', '');
+      } else if (fold !== false && (fold || ts_entry_max === 0)) {
         h1.nextUntil('h1').css('display', 'none');
       }
       return [ts_day, ts_entry_max];
@@ -50,19 +54,42 @@
     });
     /* Fold day */;
     $('.button_fold').click(function(ev) {
-      var ts_day, ts_entry_max, _ref;
-      _ref = fold_entries($(ev.target).parent('h1'), true), ts_day = _ref[0], ts_entry_max = _ref[1];
-      folds[ts_day] = Math.max(ts_entry_max, folds[ts_day] || 0);
+      var h1, ts_day, ts_entry_max, _ref;
+      h1 = $(ev.target).parent('h1');
+      _ref = fold_entries(h1, false), ts_day = _ref[0], ts_entry_max = _ref[1];
+      if (ts_entry_max > 0) {
+        fold_entries(h1, true);
+        folds[ts_day] = Math.max(ts_entry_max, folds[ts_day] || 0);
+      } else {
+        fold_entries(h1, false, true);
+        delete folds[ts_day];
+      }
       return folds_commit();
     });
     /* Fold all */;
     return $('.button_fold_all').click(function(ev) {
-      return $('h1.feed').each(function(idx, el) {
+      var h1s, ts_page_max;
+      ts_page_max = 0;
+      h1s = $('h1.feed');
+      h1s.each(function(idx, el) {
         var ts_day, ts_entry_max, _ref;
-        _ref = fold_entries(el, true), ts_day = _ref[0], ts_entry_max = _ref[1];
-        folds[ts_day] = Math.max(ts_entry_max, folds[ts_day] || 0);
-        return folds_commit();
+        _ref = fold_entries(el, false), ts_day = _ref[0], ts_entry_max = _ref[1];
+        return ts_page_max = Math.max(ts_page_max, ts_entry_max);
       });
+      if (ts_page_max > 0) {
+        h1s.each(function(idx, el) {
+          var ts_day, ts_entry_max, _ref;
+          _ref = fold_entries(el, true), ts_day = _ref[0], ts_entry_max = _ref[1];
+          return folds[ts_day] = Math.max(ts_entry_max, folds[ts_day] || 0);
+        });
+      } else {
+        h1s.each(function(idx, el) {
+          var ts_day, ts_entry_max, _ref;
+          _ref = fold_entries(el, false, true), ts_day = _ref[0], ts_entry_max = _ref[1];
+          return delete folds[ts_day];
+        });
+      }
+      return folds_commit();
     });
   });
 }).call(this);
