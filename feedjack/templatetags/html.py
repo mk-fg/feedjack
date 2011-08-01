a3e949bbf2f5ad5312ff4acc93ea57d8ca34f041
Mike Kazantsev <mk.fraggod@gmail.com>
2011-08-01 07:35:41 +0000

    styles.fern.fold: added prettyhtml filter, updated html templatetags lib to use fjlib lxml routines (if available)

--- feedjack/templatetags/html.py
+++ feedjack/templatetags/html.py
@@ -1,46 +1,44 @@
 from __future__ import unicode_literals
 import itertools as it, operator as op, functools as ft
 
+from django.template.defaultfilters import stringfilter
+from django.utils.safestring import SafeData, mark_safe
+from feedjack.fjlib import html_cleaner
+
 from django import template
 register = template.Library()
 
 
-from django.utils.encoding import smart_unicode
-from django.utils.safestring import SafeData, mark_safe
-
-def soupify(soup):
-	from BeautifulSoup import BeautifulSoup, Tag
-	return soup if isinstance(soup, (BeautifulSoup, Tag))\
-		else BeautifulSoup(soup)
-
-def desoupify(soup):
-	return mark_safe(smart_unicode(soup))
-
-
-@register.filter
-def tag_pick_text(soup, sep=' '):
-	return sep.join(soupify(soup).findAll(text=True))
-
-@register.filter
-def tag_pick(soup, tag):
-	return mark_safe(soupify(soup).find(tag))
-
-@register.filter
-def tag_attr_add(soup, attrspec):
-	if not soup: return ''
-	soup = soupify(soup)
-	for attr in attrspec.split('|'):
-		var, val = attr.split('=', 1)
-		soup[var] = val
-	return mark_safe(soup)
-
-
-from django.template.defaultfilters import stringfilter
 from django.utils.html import escape
 
 @register.filter
 @stringfilter
 def prettyhtml(value, autoescape=None):
-	value = smart_unicode(soupify(value))
+	value = html_cleaner(value)
 	return escape(value) if autoescape\
 		and not isinstance(value, SafeData) else mark_safe(value)
+
+
+# lxml is hard-dep in fern style only, at least initially
+try: from fjlib import lxml_soup, lxml_tostring
+except ImportError: pass
+else:
+	@register.filter
+	@stringfilter
+	def tag_pick_text(soup):
+		return mark_safe(lxml_soup(soup).text_content())
+
+	@register.filter
+	@stringfilter
+	def tag_pick(soup, tag):
+		return mark_safe(lxml_tostring(lxml_soup(soup).find('div')))
+
+	@register.filter
+	@stringfilter
+	def tag_attr_add(soup, attrspec):
+		if not soup: return ''
+		soup = lxml_soup(soup)
+		for attr in attrspec.split('|'):
+			var, val = attr.split('=', 1)
+			soup.attrib[var] = val
+		return mark_safe(lxml_tostring(soup))
