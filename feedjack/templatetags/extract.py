cacc8f81047ff1940e2e8175469fdbfedba28471
Mike Kazantsev <mk.fraggod@gmail.com>
2012-02-29 01:42:05 +0600

    templatetags: +extract lib, with a single method site_ordering_date to produce Site-appropriate date for Post

---
+++ feedjack/templatetags/extract.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+import itertools as it, operator as op, functools as ft
+
+from django import template
+register = template.Library()
+
+
+@register.filter
+def site_ordering_date(item, site):
+	return item.date_on_site(site)
