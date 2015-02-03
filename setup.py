2f6183dc24a7e3f40f4b4df25abf858c8aee9cc0
Mike Kazantsev <mk.fraggod@gmail.com>
2015-02-03 05:38:35 +0500

    setup: don't import full module

--- setup.py
+++ setup.py
@@ -2,33 +2,36 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
-from finddata import find_package_data
-import feedjack
-import os, sys
+from finddata import find_package_data # XXX: drop this
+import os, sys, runpy
+
+pkg_root = os.path.dirname(__file__)
+feedjack = type( 'ModuleVars', (object,),
+	runpy.run_path(os.path.join(pkg_root, 'feedjack', '__init__.py')) )
 
 package_data = find_package_data(where='feedjack', package='feedjack')
 
 # Error-handling here is to allow package to be built w/o README included
-try:
-	readme = open(os.path.join(
-		os.path.dirname(__file__), 'README.txt' )).read()
+try: readme = open(os.path.join(pkg_root, 'README.txt')).read()
 except IOError: readme = ''
 else: package_data.setdefault('', list()).append('README.txt')
 
 setup(
-	name = 'Feedjack',
-	version = feedjack.__version__,
-	author = 'Gustavo Picón, Mike Kazantsev',
-	author_email = 'gpicon@gmail.com, mk.fraggod@gmail.com',
-	license = 'BSD',
-	keywords = [ 'feed', 'aggregator', 'planet',
+
+	name='Feedjack',
+	version=feedjack.__version__,
+	author='Gustavo Picón, Mike Kazantsev',
+	author_email='gpicon@gmail.com, mk.fraggod@gmail.com',
+	license='BSD',
+	keywords=[ 'feed', 'aggregator', 'planet',
 		'rss', 'atom', 'syndication', 'django', 'feedparser', 'news' ],
-	url = feedjack.__url__,
 
-	description = 'Multisite Feed Agregator (Planet) or personal feed reader',
-	long_description = readme,
+	url=feedjack.__url__,
 
-	classifiers = [
+	description='Multisite Feed Agregator (Planet) or personal feed reader',
+	long_description=readme,
+
+	classifiers=[
 		'Development Status :: 4 - Production/Stable',
 		'Environment :: Console',
 		'Environment :: Web Environment',
@@ -49,16 +52,16 @@ setup(
 		'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
 		'Topic :: Internet :: WWW/HTTP :: WSGI :: Application' ],
 
-	install_requires = ['feedparser', 'Django >= 1.7'],
-	extras_require = {
+	install_requires=['feedparser', 'Django >= 1.7'],
+	extras_require={
 		'old_db_migration': ['South'],
 		'themes.fern': ['lxml'],
 		'themes.plain': ['lxml'] },
 
-	zip_safe = False,
-	packages = find_packages(),
-	package_data = package_data,
-	include_package_data = True,
+	zip_safe=False,
+	packages=find_packages(),
+	package_data=package_data,
+	include_package_data=True,
 
-	entry_points = {
-		'console_scripts': ['feedjack_update = feedjack.fjupdate:main'] } )
+	entry_points={
+		'console_scripts': ['feedjack_update=feedjack.fjupdate:main'] } )
