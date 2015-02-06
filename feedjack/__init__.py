5d5dc8b60630f2db136184cc1b621ba6ce825bb4
Mike Kazantsev <mk.fraggod@gmail.com>
2015-02-06 18:10:33 +0500

    Added minimal django.apps configuration

--- feedjack/__init__.py
+++ feedjack/__init__.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
 # Re-used in setup.py and fjupdate user-agent data
-__version__ = '15.02.30'
+__version__ = '15.02.31'
 __url__ = 'https://github.com/mk-fg/feedjack'
+
+# For django.apps
+default_app_config = 'feedjack.apps.FeedjackConfig'
---
+++ feedjack/apps.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function
+
+from django.apps import AppConfig
+
+
+class FeedjackConfig(AppConfig):
+
+	name = 'feedjack'
+	verbose_name = 'Feedjack'
