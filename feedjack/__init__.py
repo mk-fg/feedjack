9ca9c34ddf35d3e53d7b2d2792de496c30702895
Mike Kazantsev <mk.fraggod@gmail.com>
2015-04-29 17:01:30 +0500

    CHANGES: add note on the list not being maintained anymore

--- feedjack/__init__.py
+++ feedjack/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Re-used in setup.py and fjupdate user-agent data
-__version__ = '15.4.4'
+__version__ = '15.4.5'
 __url__ = 'https://github.com/mk-fg/feedjack'
 
 # For django.apps
